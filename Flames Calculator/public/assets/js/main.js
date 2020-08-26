5c4db32c842421b1382d29a9f58007a4142e1ce2
Kshitij Dhyani <wimpywarlord@gmail.com>
2020-08-27 01:09:21 +0530

    added logic for falmes

---
+++ Flames Calculator/public/assets/js/main.js
@@ -0,0 +1,17 @@
+function flames() {
+	var inputOne = $(".name_1").val();
+	var inputTwo = $(".name_2").val();
+	console.log(inputOne);
+	console.log(inputTwo);
+	var flames = ["Friends", "Love", "Affair", "Marriage", "Enemy", "Sister"];
+	if (inputOne != "" && inputTwo != "") {
+		var lengthInput = inputOne.length + inputTwo.length;
+		//alert(lengthInput);
+		while (flames.length != 1) {
+			flames = flames.splice(lengthInput % 6, 1);
+		}
+		$("span").html("Result:   " + flames);
+	} else {
+		alert("Fill out both the fields");
+	}
+}
