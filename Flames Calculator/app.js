d3da0609b221e54cfcbfcc4f5c64b73216668e21
Kshitij Dhyani <wimpywarlord@gmail.com>
2020-08-26 15:57:03 +0530

    added few dependensies

--- Flames Calculator/app.js
+++ Flames Calculator/app.js
@@ -4,6 +4,10 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.get("/", function (req, res) {
+	res.send("hi");
+});
+
 app.listen(process.env.PORT || 8000, function () {
-    console.log("SERVER 8000 HAS STARTED");
+	console.log("SERVER 8000 HAS STARTED");
 });
